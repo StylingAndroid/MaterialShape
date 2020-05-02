cbbefe662a12702bdf2facb9d2e0052c0eae1524
Mark Allison <mark@stylingandroid.com>
2020-05-02 17:13:23 +0100

    Code for ShapeAppearance article

---
+++ app/src/main/java/com/stylingandroid/materialshape/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.stylingandroid.materialshape
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.stylingandroid.materialshape.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
